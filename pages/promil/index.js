1d3fb4ead090dd7e865dba54b90907d9aa0a56e9
alpolcaymis <alpmis99@gmail.com>
2024-06-30 17:32:46 +0300

    ok

--- pages/promil/index.js
+++ pages/promil/index.js
@@ -1,13 +1,9 @@
 "use client";
 import React from "react";
 import MainComponent from "../../components/MainComponent";
-import FlowBanner from "@/components/FlowBanner";
-import Header from "@/components/Header";
 const PromilPage = () => {
   return (
     <>
-      <FlowBanner />
-      <Header />
       <main>
         <MainComponent />
       </main>
